5bd7c2d0b8e3889b833384161f5c53ecb41aaba3
Tyler Ryden <tryden11@gmail.com>
2022-05-15 21:26:45 -0500

    added gif to loginActivity, styled loginActivity and added extra spacing

--- app/src/main/java/com/tryden12/titanstabs/LoginActivity.kt
+++ app/src/main/java/com/tryden12/titanstabs/LoginActivity.kt
@@ -1,11 +1,14 @@
 package com.tryden12.titanstabs
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.text.method.PasswordTransformationMethod
+import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.tryden12.titanstabs.data.database.AppDatabase
@@ -38,6 +41,8 @@ class LoginActivity : AppCompatActivity() {
         // Transform password to dots
         binding.editTextPassword.transformationMethod = PasswordTransformationMethod()
 
+
+        setStatusBar()
 
     }
     /*** Method for Testing ***********************************/
@@ -76,6 +81,21 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+
+
+
+    private fun setStatusBar() {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+        )
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+    }
 
 
 
