1b095d26932d57841d22baf8a4b479cff6287cc9
Tyler Ryden <tryden11@gmail.com>
2022-05-16 11:09:26 -0500

    added toggle menu code, set status bar to dark blue, and status bar text to white

--- app/src/main/java/com/tryden12/titanstabs/MainActivity.kt
+++ app/src/main/java/com/tryden12/titanstabs/MainActivity.kt
@@ -2,10 +2,45 @@ package com.tryden12.titanstabs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.tryden12.titanstabs.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle :ActionBarDrawerToggle
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toggleMenu()
+        window.statusBarColor = resources.getColor(R.color.dark_blue)
+
+    }
+
+
+
+
+    fun toggleMenu() {
+        /***************** Toggle Menu ****************************************************/
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_menu1 -> Toast.makeText(applicationContext,
+                    "Clicked item 1", Toast.LENGTH_SHORT).show()
+                R.id.action_menu2 -> Toast.makeText(applicationContext,
+                    "Clicked item 2", Toast.LENGTH_SHORT).show()
+                R.id.action_menu3 -> Toast.makeText(applicationContext,
+                    "Clicked item 3", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
 }
