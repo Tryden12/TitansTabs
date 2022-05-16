ac6eae991e32ac4b0a243578bb8aabcbd51d7ca8
Tyler Ryden <tryden11@gmail.com>
2022-05-16 11:02:05 -0500

    coverted splashScreen class from java to kotlin and added a translucent status & nav bar feature

--- app/src/main/java/com/tryden12/titanstabs/SplashScreen.kt
+++ app/src/main/java/com/tryden12/titanstabs/SplashScreen.kt
@@ -1,20 +1,37 @@
-package com.tryden12.titanstabs;
+package com.tryden12.titanstabs
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.graphics.Color
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import com.tryden12.titanstabs.LoginActivity
 
-import androidx.appcompat.app.AppCompatActivity;
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setStatusBar()
+        super.onCreate(savedInstanceState)
 
-public class SplashScreen extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreen, LoginActivity::class.java))
+            finish()
+        }, 2000)
+    }
 
-        new Handler().postDelayed(() -> {
-            startActivity(new Intent(SplashScreen.this, LoginActivity.class));
-            finish();
-        }, 2000);
+
+    private fun setStatusBar() {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+        )
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
     }
 }
