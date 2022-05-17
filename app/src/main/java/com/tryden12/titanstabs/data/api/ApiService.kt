b2577f00df98259d5e3202d6f93e20dd98e29014
Tyler Ryden <tryden11@gmail.com>
2022-05-17 12:00:18 -0500

    renamed player model to players and changed the class to a data class

--- app/src/main/java/com/tryden12/titanstabs/data/api/ApiService.kt
+++ app/src/main/java/com/tryden12/titanstabs/data/api/ApiService.kt
@@ -2,11 +2,12 @@ package com.tryden12.titanstabs.data.api
 
 import android.telecom.Call
 import com.tryden12.titanstabs.data.model.Player
+import com.tryden12.titanstabs.data.model.Players
 import retrofit2.http.GET
 
 interface ApiService {
 
     @GET
-    fun getPlayer() : Player
+    fun getPlayers() : Players
 
 }
