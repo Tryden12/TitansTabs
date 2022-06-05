6dbc9031855aabd59b235c6282fe7dc9f525b364
Tyler Ryden <tryden11@gmail.com>
2022-06-05 13:46:14 -0500

    testing dataRepository

--- app/src/main/java/com/tryden12/titanstabs/ui/main/viewmodel/ViewModel.kt
+++ app/src/main/java/com/tryden12/titanstabs/ui/main/viewmodel/ViewModel.kt
@@ -2,9 +2,13 @@ package com.tryden12.titanstabs.ui.main.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tryden12.titanstabs.data.manualParsing.ManualParsingImpl
 import com.tryden12.titanstabs.data.repository.DataRepository
 import com.tryden12.titanstabs.data.model.Player
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ViewModel : ViewModel() {
     // Test ViewModel with text below
@@ -12,17 +16,21 @@ class ViewModel : ViewModel() {
 
     private val dataRepository = DataRepository(ManualParsingImpl())
 
-/*
-    fun fetchPlayer() {
+
+    suspend fun fetchPlayer() : Player {
+        return dataRepository.getPlayers().player[0]
+        /*
         viewModelScope.launch {
             val player : Player = withContext(Dispatchers.IO) {
                 dataRepository.getPlayers().player[0]
             }
             playerLiveData.value = player
         }
+
+         */
     }
 
- */
+
 
 
 
