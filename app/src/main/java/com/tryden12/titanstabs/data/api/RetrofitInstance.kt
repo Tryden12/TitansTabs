a6a404933aa7cfef7c7dd71ccdae9827d751cb7d
Tyler Ryden <tryden11@gmail.com>
2022-07-09 14:11:01 -0500

    updated base url

--- app/src/main/java/com/tryden12/titanstabs/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/tryden12/titanstabs/data/api/RetrofitInstance.kt
@@ -11,7 +11,7 @@ object RetrofitInstance {
 
     // Base Url
     private const val BASE_URL: String =
-        "https://www.thesportsdb.com/api/v1/json/50130162/searchplayers.php?t=" // Tennessee%Titans
+        "https://www.thesportsdb.com/api/v1/json/50130162/" // searchplayers.php?t=Tennessee%Titans
 
     // Create Moshi instance
     private val moshi = Moshi.Builder()
