efdc80b14848de65f5c5660206bebbccc1ac81a4
Tyler Ryden <tryden11@gmail.com>
2022-07-09 14:11:30 -0500

    ready to test viewModel

--- app/src/main/java/com/tryden12/titanstabs/ui/main/viewmodel/ViewModel.kt
+++ app/src/main/java/com/tryden12/titanstabs/ui/main/viewmodel/ViewModel.kt
@@ -1,20 +1,56 @@
 package com.tryden12.titanstabs.ui.main.viewmodel
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tryden12.titanstabs.data.api.RetrofitInstance.apiService
 import com.tryden12.titanstabs.data.repository.DataRepository
 import com.tryden12.titanstabs.data.model.Player
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ViewModel : ViewModel() {
 
-    // Test ViewModel with text below
-    val textHello = "Some text from ViewModel"
-
     private val dataRepository = DataRepository()
+
+    // Live data for the list from Api
+    var liveDataList: MutableLiveData<MutableList<Player>> = MutableLiveData()
+
+    // Live data for each item
+    var liveItemData: MutableLiveData<Player> = MutableLiveData()
+
+    // Observer for live list
+    fun getLiveDataObserver(): MutableLiveData<MutableList<Player>> {
+        return liveDataList
+    }
+
+    // Observer for each item
+    fun getLiveItemObserver(): MutableLiveData<Player> {
+        return liveItemData
+    }
+
+    // Make Api call for list data
+    fun makeApiCall() {
+        val call = apiService.getPlayers()
+        call.enqueue(object : Callback<MutableList<Player>> {
+            override fun onResponse(
+                call: Call<MutableList<Player>>,
+                response: Response<MutableList<Player>>
+            ) {
+                liveDataList.postValue(response.body())
+            }
+
+            @SuppressLint("NullSafeMutableLiveData")
+            override fun onFailure(call: Call<MutableList<Player>>, t: Throwable) {
+                liveDataList.postValue(null)
+            }
+        })
+    }
 
 /*
     fun fetchPlayer() : Player {
