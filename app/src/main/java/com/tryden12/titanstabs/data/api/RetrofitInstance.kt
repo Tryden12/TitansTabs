5a45118e7c12e00475c134e84e094afa864ccf84
Tyler Ryden <tryden11@gmail.com>
2022-07-09 14:31:36 -0500

    added gson for testing

--- app/src/main/java/com/tryden12/titanstabs/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/tryden12/titanstabs/data/api/RetrofitInstance.kt
@@ -1,7 +1,10 @@
 package com.tryden12.titanstabs.data.api
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -18,6 +21,10 @@ object RetrofitInstance {
         .addLast(KotlinJsonAdapterFactory())
         .build()!!
 
+    private val gson: Gson by lazy {
+        GsonBuilder().setLenient().create()
+    }
+
     // Http Client
     private val httpClient: OkHttpClient by lazy {
         OkHttpClient.Builder().build()
@@ -28,7 +35,7 @@ object RetrofitInstance {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(httpClient)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
 
