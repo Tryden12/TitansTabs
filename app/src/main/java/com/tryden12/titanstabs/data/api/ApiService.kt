64467c3c7e1f1fde00ce6050b4b726fe0a0528b1
Tyler Ryden <tryden11@gmail.com>
2022-07-09 14:10:27 -0500

    updated @GET

--- app/src/main/java/com/tryden12/titanstabs/data/api/ApiService.kt
+++ app/src/main/java/com/tryden12/titanstabs/data/api/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("Tennessee%Titans")
+    @GET("searchplayers.php?t=Tennessee%Titans")
     fun getPlayers() : Call<MutableList<Player>>
 
 }
