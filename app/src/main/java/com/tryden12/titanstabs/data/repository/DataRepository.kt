e4220fdcb04c4c13590bae8d22c6329224a90a2a
Tyler Ryden <tryden11@gmail.com>
2022-07-09 14:39:43 -0500

    testing

--- app/src/main/java/com/tryden12/titanstabs/data/repository/DataRepository.kt
+++ app/src/main/java/com/tryden12/titanstabs/data/repository/DataRepository.kt
@@ -1,14 +1,17 @@
 package com.tryden12.titanstabs.data.repository
 
+import com.tryden12.titanstabs.data.api.RetrofitInstance.apiService
+import com.tryden12.titanstabs.data.model.Player
 import com.tryden12.titanstabs.data.model.Players
+import retrofit2.Call
 
 class DataRepository() {
 
-    /*
-    fun getPlayers() : Players {
-        return playersApi.getPlayers()
+
+    fun getPlayers() : Call<MutableList<Player>> {
+        return apiService.getPlayers()
     }
 
 
-     */
+
 }
