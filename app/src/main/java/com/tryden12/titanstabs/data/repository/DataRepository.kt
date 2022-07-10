c0900ba5826fc5b77572c18d561e56ad0df3ce18
Tyler Ryden <tryden11@gmail.com>
2022-07-09 21:17:20 -0500

    cleanup imports

--- app/src/main/java/com/tryden12/titanstabs/data/repository/DataRepository.kt
+++ app/src/main/java/com/tryden12/titanstabs/data/repository/DataRepository.kt
@@ -2,7 +2,6 @@ package com.tryden12.titanstabs.data.repository
 
 import com.tryden12.titanstabs.data.api.RetrofitInstance.apiService
 import com.tryden12.titanstabs.data.model.Player
-import com.tryden12.titanstabs.data.model.Players
 import retrofit2.Call
 
 class DataRepository() {
