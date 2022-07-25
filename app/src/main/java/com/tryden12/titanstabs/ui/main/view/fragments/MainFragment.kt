7fc57cf9b367ec99e09c75c0fe731a29b7c998ed
Tyler Ryden <tryden11@gmail.com>
2022-07-25 11:58:09 -0500

    setup to send/retrieve args from MainFragment about dash icons

--- app/src/main/java/com/tryden12/titanstabs/ui/main/view/fragments/MainFragment.kt
+++ app/src/main/java/com/tryden12/titanstabs/ui/main/view/fragments/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.tryden12.titanstabs.ui.main.view.fragments
 
+import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -14,9 +15,8 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.tryden12.titanstabs.R
 import com.tryden12.titanstabs.databinding.FragmentMainBinding
-import com.tryden12.titanstabs.adapter.Adapter
 import com.tryden12.titanstabs.ui.main.viewmodel.ViewModel
-
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
 class MainFragment : Fragment(), View.OnClickListener {
@@ -25,6 +25,7 @@ class MainFragment : Fragment(), View.OnClickListener {
 
     var navController : NavController? = null
 
+    private val TAG: String = MainFragment::class.java.name;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -75,22 +76,122 @@ class MainFragment : Fragment(), View.OnClickListener {
 
             // Edit button in dashboard
             R.id.edit_icon_imageView -> {
-                var dialog = Top5PopUpFragment()
+                // Send list of icons current on dashboard
+                newInstance(populateIconList())
 
+                // Open Top5PopUpFragment dialog
+                var dialog = Top5PopUpFragment()
                 dialog.show(childFragmentManager, "customDialog")
             }
             R.id.edit_icon_textview -> {
+                // Send list of icons current on dashboard
+                newInstance(populateIconList())
+
+                // Open Top5PopUpFragment dialog
                 var dialog = Top5PopUpFragment()
-                /*
-                Bundle().apply {
-                    putInt(KEY, imageDrawable)
-                }
-
-                 */
-
                 dialog.show(childFragmentManager, "customDialog")
             }
         }
+    }
+
+    private fun populateIconList() : MutableList<String> {
+        var iconList = mutableListOf<String>()
+
+        // Roster icon
+        if (roster_dash_layout.visibility == View.VISIBLE) {
+            iconList.add(getString(R.string.roster))
+        } else {
+            if (iconList.contains(getString(R.string.roster))) {
+                iconList.remove(getString(R.string.roster))
+            }
+        }
+
+        // Schedule icon
+        if (schedule_dash_layout.visibility == View.VISIBLE) {
+            iconList.add(getString(R.string.schedule))
+        } else {
+            if (iconList.contains(getString(R.string.schedule))) {
+                iconList.remove(getString(R.string.schedule))
+            }
+        }
+
+        // Depth Chart icon
+        if (depth_chart_dash_layout.visibility == View.VISIBLE) {
+            iconList.add(getString(R.string.depth_chart))
+        } else {
+            if (iconList.contains(getString(R.string.depth_chart))) {
+                iconList.remove(getString(R.string.depth_chart))
+            }
+        }
+
+        // Stats icon
+        if (stats_dash_layout.visibility == View.VISIBLE) {
+            iconList.add(getString(R.string.stats))
+        } else {
+            if (iconList.contains(getString(R.string.stats))) {
+                iconList.remove(getString(R.string.stats))
+            }
+        }
+
+        // Photos icon
+        if (photos_dash_layout.visibility == View.VISIBLE) {
+            iconList.add(getString(R.string.photos))
+        } else {
+            if (iconList.contains(getString(R.string.photos))) {
+                iconList.remove(getString(R.string.photos))
+            }
+        }
+
+        // Roster Moves icon
+        if (roster_moves_dash_layout.visibility == View.VISIBLE) {
+            iconList.add(getString(R.string.roster_moves))
+        } else {
+            if (iconList.contains(getString(R.string.roster_moves))) {
+                iconList.remove(getString(R.string.roster_moves))
+            }
+        }
+
+        // Shop icon
+        if (shop_dash_layout.visibility == View.VISIBLE) {
+            iconList.add(getString(R.string.shop))
+        } else {
+            if (iconList.contains(getString(R.string.shop))) {
+                iconList.remove(getString(R.string.shop))
+            }
+        }
+
+        // Podcasts icon
+        if (podcast_dash_layout.visibility == View.VISIBLE) {
+            iconList.add(getString(R.string.podcasts))
+        } else {
+            if (iconList.contains(getString(R.string.podcasts))) {
+                iconList.remove(getString(R.string.podcasts))
+            }
+        }
+
+        Log.d(TAG, "iconlist = $iconList")
+        return iconList
+    }
+
+    // send data to popup frag
+    fun newInstance(iconList: MutableList<String>): Top5PopUpFragment? {
+
+        val fragment = Top5PopUpFragment()
+
+        // Supply list of icons to fragment
+        val args = Bundle().apply {
+            putString("0", iconList[0])
+            putString("1", iconList[1])
+            putString("2", iconList[2])
+            putString("3", iconList[3])
+            putString("4", iconList[4])
+        }
+
+        fragment.arguments = args
+
+        Log.d(TAG, "iconlist[0] = " + iconList[0])
+        Log.d(TAG, "iconlist[1] = " + iconList[1])
+        return fragment
     }
 
     private fun disableOnBackPressed() {
--- app/src/main/java/com/tryden12/titanstabs/ui/main/view/fragments/Top5PopUpFragment.kt
+++ app/src/main/java/com/tryden12/titanstabs/ui/main/view/fragments/Top5PopUpFragment.kt
@@ -3,21 +3,26 @@ package com.tryden12.titanstabs.ui.main.view.fragments
 import android.content.res.Resources
 import android.graphics.Rect
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import com.tryden12.titanstabs.R
-import com.tryden12.titanstabs.databinding.FragmentScheduleBinding
 import com.tryden12.titanstabs.databinding.FragmentTop5PopUpBinding
 
 
-class Top5PopUpFragment : DialogFragment() {
+class Top5PopUpFragment : DialogFragment(), View.OnClickListener {
+    private val TAG: String = Top5PopUpFragment::class.java.name;
     private lateinit var binding: FragmentTop5PopUpBinding
     var navController: NavController? = null
+
+    var iconPos0: String = ""
+    var iconPos1: String = ""
+    var iconPos2: String = ""
+    var iconPos3: String = ""
+    var iconPos4: String = ""
 
 
     override fun onCreateView(
@@ -34,6 +39,10 @@ class Top5PopUpFragment : DialogFragment() {
        // navController = Navigation.findNavController(view)
         setWidthPercent(90)
 
+        // Set icon list with correct drawable
+        retrieveIconList()
+        setIconList(retrieveIconList())
+
     }
 
     // Set the width of the dialog to a percentage of the current screen width
@@ -48,5 +57,60 @@ class Top5PopUpFragment : DialogFragment() {
         if (dialog != null && dialog?.window != null) {
             dialog!!.window?.setBackgroundDrawable(resources.getDrawable(R.drawable.custom_dialog_bg_top_5));
         }
+    }
+
+    private fun retrieveIconList(): MutableList<String> {
+        // Retrieve icon data
+        iconPos0 = arguments?.getString("0").toString()
+        iconPos1 = arguments?.getString("1").toString()
+        iconPos2 = arguments?.getString("2").toString()
+        iconPos3 = arguments?.getString("3").toString()
+        iconPos4 = arguments?.getString("4").toString()
+
+        Log.d(TAG, "iconPos0 = " + iconPos0) // returning null, should return "Roster"
+        // Create list of active icons
+        var iconList = mutableListOf<String>()
+        // Add to list
+        iconList.add(iconPos0!!)
+        iconList.add(iconPos1!!)
+        iconList.add(iconPos2!!)
+        iconList.add(iconPos3!!)
+        iconList.add(iconPos4!!)
+
+        Log.d(TAG, "iconlist[0] = " + iconList[0]) // returning null
+        return iconList
+    }
+
+
+    private fun setIconList(iconList: MutableList<String>){
+
+        if (iconList.contains(getString(R.string.roster))) {
+            binding.radioButtonRosterPick.setBackgroundResource(R.drawable.icon_helmet_blue_bg)
+        }
+        if (iconList.contains(getString(R.string.schedule))) {
+            binding.radioButtonSchedulePick.setBackgroundResource(R.drawable.icon_schedule_blue_bg)
+        }
+        if (iconList.contains(getString(R.string.depth_chart))) {
+            binding.radioButtonDepthChart.setBackgroundResource(R.drawable.icon_depth_chart_blue_bg)
+        }
+        if (iconList.contains(getString(R.string.stats))) {
+            binding.radioButtonStatsPick.setBackgroundResource(R.drawable.icon_stats_blue_bg)
+        }
+        if (iconList.contains(getString(R.string.photos))) {
+            binding.radioButtonPhotosPick.setBackgroundResource(R.drawable.icon_photos_blue_bg)
+        }
+        if (iconList.contains(getString(R.string.roster_moves))) {
+            binding.radioButtonRosterMovesPick.setBackgroundResource(R.drawable.icon_roster_moves_blue_bg)
+        }
+        if (iconList.contains(getString(R.string.shop))) {
+            binding.radioButtonShopPick.setBackgroundResource(R.drawable.icon_shop_blue_bg)
+        }
+        if (iconList.contains(getString(R.string.podcasts))) {
+            binding.radioButtonPodcastPick.setBackgroundResource(R.drawable.icon_podcast_blue_bg)
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
