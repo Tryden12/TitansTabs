9c6d38d66ddd3c44456cd7715ecd6e7501313aa1
Tyler Ryden <tryden11@gmail.com>
2022-07-25 11:56:19 -0500

    created model for dashicons

---
+++ app/src/main/java/com/tryden12/titanstabs/data/model/DashIcons.kt
@@ -0,0 +1,11 @@
+package com.tryden12.titanstabs.data.model
+
+data class DashIcons(
+
+    val iconPos0 : String? = null,
+    val iconPos1 : String? = null,
+    val iconPos2 : String? = null,
+    val iconPos3 : String? = null,
+    val iconPos4 : String? = null,
+
+)
